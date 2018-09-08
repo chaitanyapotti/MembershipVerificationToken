2937b43e5fda6b8a0dfc26b1b26aacc332610f1b
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-08 15:27:00 +0530

    protocol phase 1

--- contracts/ElectusProtocol.sol
+++ contracts/ElectusProtocol.sol
@@ -1,12 +1,88 @@
 pragma solidity ^0.4.24;
 
+
+//For truffle compilation, use path zeppelin-solidity/contracts/ownership/Ownable.sol
+//For linting purposes, use path zeppelin-solidity/ownership/Ownable.sol
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/introspection/SupportsInterfaceWithLookup.sol";
 import "./Protocol/IElectusProtocol.sol";
 
 
-contract ElectusProtocol is ERC1261, Ownable, ERC165 {
-    
+contract ElectusProtocol is IERC1261, Ownable, SupportsInterfaceWithLookup {
+    struct MemberData{
+        bool hasToken;
+        bytes32[] data;
+    }
+
+    mapping(address => MemberData) public currentHolders;
+
+    address[] public allHolders;
 
     event Assigned(address indexed to);
     event Revoked(address indexed to);
+
+    constructor () public {
+        supportedInterfaces[0x1325] = true; //IERC1261 --todo
+        supportedInterfaces[0x124] = true; //Ownable --todo
+    }
+
+    modifier isCurrentHolder {
+        require(isCurrentMember(msg.sender), "Not a current member");
+        _;
+    }
+
+     modifier isNotACurrentHolder {
+        require(!isCurrentMember(msg.sender), "Already a member");
+        _;
+    }
+
+    function isCurrentMember(address _to) public view returns (bool){
+        return currentHolders[_to].hasToken;
+    }
+
+    function getAllMembers() external view returns (address[]) {
+        return allHolders;
+    }
+
+    function totalMemberCount() external view returns (uint) {
+        return allHolders.length;
+    }
+
+    function getData(address _to) external view returns (bytes32[]) {
+        return currentHolders[_to].data;
+    }
+
+    function modifyData(address _to, bytes32[] newData) external onlyOwner {
+        currentHolders[_to].data = newData;
+    }
+
+    function requestMembership(bytes32[] data) external isNotACurrentHolder payable {
+        //Do some checks before assigning membership
+        _assign(msg.sender, data);
+    }
+
+    function revokeMembership() external isCurrentHolder payable {
+        _revoke(msg.sender);
+    }
+
+    function assignTo(address _to, bytes32[] data) external onlyOwner {
+        _assign(_to, data);
+    }
+
+    function revokeFrom(address _from) external onlyOwner {
+        _revoke(_from);
+    }
+
+    function _assign(address _to, bytes32[] data) private {
+        MemberData memory member = MemberData({hasToken: true, data: data});
+        currentHolders[_to] = member;
+        allHolders.push(_to);
+        emit Assigned(_to);
+    }
+
+    function _revoke(address _from) private {
+        MemberData storage member = currentHolders[_from];
+        member.hasToken = false;
+        emit Revoked(_from);
+    }    
 }
--- contracts/Protocol/IElectusProtocol.sol
+++ contracts/Protocol/IElectusProtocol.sol
@@ -1,35 +1,11 @@
 pragma solidity ^0.4.24;
 
-
-interface ERC165 {
-    /// @notice Query if a contract implements an interface
-    /// @param interfaceID The interface identifier, as specified in ERC-165
-    /// @dev Interface identification is specified in ERC-165. This function
-    ///  uses less than 30,000 gas.
-    /// @return `true` if the contract implements `interfaceID` and
-    ///  `interfaceID` is not 0xffffffff, `false` otherwise
-    function supportsInterface(bytes4 interfaceID) external view returns (bool);
-}
-
-/// @title ERC-173 Contract Ownership Standard
-/// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-173.md
-///  Note: the ERC-165 identifier for this interface is 0x7f5828d0
-interface ERC173 /* is ERC165 */ {
-    /// @dev This emits when ownership of a contract changes.    
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /// @notice Get the address of the owner    
-    /// @return The address of the owner.
-    function owner() view external;
-	
-    /// @notice Set the address of the new owner of the contract   
-    /// @param _newOwner The address of the new owner of the contract    
-    function transferOwnership(address _newOwner) external;	
-}
+import "zeppelin-solidity/contracts/introspection/ERC165.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 //todo: add interoperability with other membership tokens
 //All other contracts must use a 1300 token which implements this protocol - only reference
-interface ERC1261 /* is ERC173, ERC165 */ {
+interface IERC1261 /* is ERC173, ERC165 */ {
 
     event Assigned(address indexed to);
     event Revoked(address indexed to);
@@ -48,7 +24,7 @@ interface ERC1261 /* is ERC173, ERC165 */ {
 /// @title ERC-1261 Membership Verification Token Standard, optional metadata extension
 /// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1261.md
 ///  Note: the ERC-1261 identifier for this interface is //TODO.
-interface ERC1261Metadata /* is ERC1261 */ {
+interface IERC1261Metadata /* is ERC1261 */ {
     /// @notice A descriptive name for a collection of MTs in this contract
     function name() external view returns (string _name);
 
