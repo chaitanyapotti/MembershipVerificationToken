f700332409fdaceb837284241e4afb09821590b3
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-08 14:38:05 +0530

    npm install

--- contracts/ElectusProtocol.sol
+++ contracts/ElectusProtocol.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.24;
 
-import "zeppelin/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./Protocol/IElectusProtocol.sol";
 
 
-contract ElectusProtocol is ERC1261, ERC173, ERC165 {
+contract ElectusProtocol is ERC1261, Ownable, ERC165 {
     
-    
+
     event Assigned(address indexed to);
     event Revoked(address indexed to);
 }
