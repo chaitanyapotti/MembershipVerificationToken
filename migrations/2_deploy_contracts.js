6422e3126ba3f51e64e4a2dafdf2371544984f99
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-15 11:32:04 +0530

    removed gas constraints

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ var ERC1261MetaData = artifacts.require("./ERC1261MetaData.sol");
 //No need to deploy interfaces ...only contracts
 
 module.exports = function(deployer) {
-  deployer.deploy(ElectusProtocol, { gas: 2400000 });
+  deployer.deploy(ElectusProtocol);
   deployer.link(ElectusProtocol, ERC1261MetaData);
   deployer.deploy(ERC1261MetaData);
 };
