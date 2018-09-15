0424b7f0b542cfa11c0babbb21ff0da191bb676a
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-15 11:34:13 +0530

    compile fixes

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   testrpcOptions: `--port 8555 -i 1999 --noVMErrorsOnRPCResponse`,
   testCommand: "truffle test --network coverage",
-  compileCommand: "../node_modules/.bin/truffle compile",
+  compileCommand: "truffle compile",
   copyPackages: ["openzeppelin-solidity"]
 };
