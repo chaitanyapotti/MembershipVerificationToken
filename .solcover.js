d2ffe92dc28bfd95ff43ecca42e9dfcd907a1f00
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-15 12:11:46 +0530

    test changes

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   norpc: true,
-  testCommand: "node --max-old-space-size=4096 truffle test --network coverage",
+  testCommand: "truffle test --network coverage",
   compileCommand: "truffle compile --network coverage",
   copyPackages: ["openzeppelin-solidity", "ganache-cli"]
 };
