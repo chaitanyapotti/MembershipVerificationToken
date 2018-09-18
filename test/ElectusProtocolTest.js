305486f7ece4ae64075f2960a9f2175310cb7874
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-18 21:16:32 +0530

    eslint disable rule

--- test/ElectusProtocolTest.js
+++ test/ElectusProtocolTest.js
@@ -24,25 +24,30 @@ contract("ElectusProtocol", function(accounts) {
   });
   it("list of attributes Names", async () => {
     const data = await electusProtocol.getAttributeNames();
-    assert.equal(web3.toAscii(data[1]).replace("/\u0000/g", ""), "skin", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(data[1]).replace(/\u0000/g, ""), "skin", 32);
   });
   it("list of attributes of a member", async () => {
     const data = await electusProtocol.getAttributes(accounts[1]);
-    assert.equal(web3.toAscii(data[0]).replace("/\u0000/g", ""), "black", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(data[0]).replace(/\u0000/g, ""), "black", 32);
   });
   it("gets attribute of a member by name", async () => {
     const data = await electusProtocol.getAttributeByName(accounts[1], "hair");
-    assert.equal(web3.toAscii(data).replace("/\u0000/g", ""), "black", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(data).replace(/\u0000/g, ""), "black", 32);
   });
   it("adds a set of attributes", async () => {
     await electusProtocol.addAttributeSet("height", ["5", "6"]);
     const data = await electusProtocol.getAttributeNames();
-    assert.equal(web3.toAscii(data[2]).replace("/\u0000/g", ""), "height", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(data[2]).replace(/\u0000/g, ""), "height", 32);
   });
   it("modifies attribute by name", async () => {
     await electusProtocol.modifyAttributeByName(accounts[1], "hair", 0);
     const data = await electusProtocol.getAttributes(accounts[1]);
-    assert.equal(web3.toAscii(data[0]).replace("/\u0000/g", ""), "black", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(data[0]).replace(/\u0000/g, ""), "black", 32);
   });
   it("request memebership", async () => {
     await electusProtocol.requestMembership([0], {
@@ -51,7 +56,8 @@ contract("ElectusProtocol", function(accounts) {
     const data = await electusProtocol.getAllMembers();
     const attr = await electusProtocol.getAttributes(accounts[2]);
     assert.equal(data[1], accounts[2]);
-    assert.equal(web3.toAscii(attr[0]).replace("/\u0000/g", ""), "black", 32);
+    // eslint-disable-next-line no-control-regex
+    assert.equal(web3.toAscii(attr[0]).replace(/\u0000/g, ""), "black", 32);
   });
   it("self revoke memebership", async () => {
     await electusProtocol.forfeitMembership({
