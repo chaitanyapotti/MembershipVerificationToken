c5f989b036f2b048d23b1ffe419657ef056ba444
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-21 18:09:05 +0530

    fix for coverage

--- truffle-config.js
+++ truffle-config.js
@@ -20,14 +20,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    coverage: {
+      host: "localhost",
+      port: 8555, // <-- Use port 8555
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01, // <-- Use this low gas price
+      network_id: "1999"
     }
-  },
-  coverage: {
-    host: "localhost",
-    port: 8555, // <-- Use port 8555
-    gas: 0xfffffffffff, // <-- Use this high gas value
-    gasPrice: 0x01, // <-- Use this low gas price
-    network_id: "1999"
   },
   mocha: {
     useColors: true
